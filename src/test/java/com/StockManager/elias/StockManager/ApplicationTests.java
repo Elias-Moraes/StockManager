8969d243bcf4f5c175ac7b2b90321f9380faff4b
Elias <eliasmoraes0089@gmail.com>
2023-12-30 16:34:40 -0300

    arquivo base
    v-1

---
+++ src/test/java/com/StockManager/elias/StockManager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockManager.elias.StockManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
